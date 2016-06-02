461eedef4a51102cff43a703aa4f0ae42f8db6eb
ystoto <ystoto@gmail.com>
2016-06-02 11:48:07 +0000

    Add stub related to NewRequest

--- command.php
+++ command.php
@@ -2,11 +2,11 @@
 
 class ret_enum {
 	const RET_OK = 0;
-	const RET_FAIL = 1;
-	const RET_NETWORK_ERROR = 2;
-	const RET_DATABASE_ERROR = 3;
-	const RET_DATABASE_DUPLICATED_ERROR = 4;
-	const RET_PARAM_ERROR = 5;
+	const RET_FAIL = -1;
+	const RET_NETWORK_ERROR = -2;
+	const RET_DATABASE_ERROR = -3;
+	const RET_DATABASE_DUPLICATED_ERROR = -4;
+	const RET_PARAM_ERROR = -5;
 }
 
 class DB{
@@ -14,11 +14,25 @@ class DB{
 	const id = "root";
 	const pw = "se2016";
 	const name = "LTS";
-	const table = "members";
+	const member_table = "members";
+	const work_table = "workitems";
 }
 
-function connect_db(&$link) {
-        $retval = ret_enum::RET_OK;
+class MEMBER{
+	const REQUESTER = 0;
+	const TRANSLATOR = 1;
+	const REVIEWER = 2;
+}
+
+class LANG{
+	const KOREAN = 0;
+	const ENGLISH = 1;
+	const CHINESE = 2;
+	const JAPANESE = 3;
+}
+
+function connect_db(/*OUTPUT*/ &$link) {
+        $ret = ret_enum::RET_OK;
         $link = mysqli_connect(DB::ip, DB::id, DB::pw, DB::name);
         if ($dberr=mysqli_connect_errno()) {
                 error_log("mysqli_connect error = $dberr", 0);
@@ -32,13 +46,13 @@ function connect_db(&$link) {
 }
 
 function register(&$var) {
-	$retval = ret_enum::RET_OK;
+	$ret = ret_enum::RET_OK;
 	if (!isset($var->{'id'}))
 		return ret_enum::RET_PARAM_ERROR;
-	if (($retval = connect_db($link)) != ret_enum::RET_OK)
-		return $retval;
+	if (($ret = connect_db($link)) != ret_enum::RET_OK)
+		return $ret;
 
-	$query = "insert into `".DB::table."` (";
+	$query = "insert into `".DB::member_table."` (";
 	$field = array('password', 'id', 'first_name', 'family_name', 'email',
 			'phone', 'country', 'sex', 'birthday', 'user_mode',
 			'degree', 'college', 'graduate', 'certification',
@@ -67,23 +81,23 @@ function register(&$var) {
 		if (mysqli_errno($link) == 1062){
 			$id = $var->{'id'};
 			error_log("Duplicated ID : $id", 0);
-			$retval = ret_enum::RET_DATABASE_DUPLICATED_ERROR;
+			$ret = ret_enum::RET_DATABASE_DUPLICATED_ERROR;
 		}else{
 			error_log("ERROR!!", 0);
-			$retval = ret_enum::RET_DATABASE_ERROR;
+			$ret = ret_enum::RET_DATABASE_ERROR;
 		}
 	}
 	mysqli_close($link);
-	return $retval;
+	return $ret;
 }
 
 
 function login(&$var) {
-        $retval = ret_enum::RET_OK;
-        if (($retval = connect_db($link)) != ret_enum::RET_OK)
-                return $retval;
+        $ret = ret_enum::RET_OK;
+        if (($ret = connect_db($link)) != ret_enum::RET_OK)
+                return $ret;
         
-	$query = "SELECT `password` FROM `".DB::table."` WHERE `id` = '".$var->{'id'}."'";
+	$query = "SELECT `password` FROM `".DB::member_table."` WHERE `id` = '".$var->{'id'}."'";
         error_log("query: $query", 0);
 
         $result = mysqli_query($link, $query);
@@ -92,78 +106,123 @@ function login(&$var) {
 	$hashed_password = hash("SHA256", $var->{'password'});
 	if (strcmp($row['password'], $hashed_password) != 0) {
 		error_log("Wrong password!! INP : ".$hashed_password.", pw : ".$var->{'password'}, 0);
-		$retval = ret_enum::RET_PARAM_ERROR;
+		$ret = ret_enum::RET_PARAM_ERROR;
 	}
         
 	mysqli_close($link);
-        return $retval;
+        return $ret;
 }
-/*
-function logout(&$var) {
-	NO NEED!!	
-}
-*/
 
-function getUserMode(&$var) {
+function INSERT(&$var, $table) {
+	$ret = ret_enum::RET_OK;
         if (($result = connect_db($link)) != ret_enum::RET_OK) {
                 return $result;
+        }
+
+	//// Generate mysql insert sentence
+        $query = "INSERT INTO `".$table."` (";
+
+	// Count elements
+	$cnt = 0;
+	foreach($var as $key) $cnt++;
+
+	// Fill Columns
+	$idx = 1;
+	foreach($var as $key => $value) {
+                $query .= "`".$key."`";
+                if(($idx++) < $cnt)
+                        $query .= ",";
 	}
+        $query .=") VALUES (";
 
-        $query = "SELECT `user_mode` FROM `".DB::table."` WHERE `id` = '".$var->{'id'}."'";
+	// Fill Values
+	$idx = 1;
+	foreach($var as $key => $value) {
+                $query .= "'".$value."'";
+                if(($idx++) < $cnt)
+                        $query .= ",";
+	}
+        $query .=")";
+        error_log("count: ".$cnt.", query: $query", 0);
+
+        $result = mysqli_query($link, $query);
+        if ($result == false) {
+                if (mysqli_errno($link) == 1062){
+                        $id = $var->{'id'};
+                        error_log("Duplicated ID : $id", 0);
+                        $ret = ret_enum::RET_DATABASE_DUPLICATED_ERROR;
+                }else{
+                        error_log("ERROR!!", 0);
+                        $ret = ret_enum::RET_DATABASE_ERROR;
+                }
+        }
+        mysqli_close($link);
+        return $ret;
+}
+
+
+function SELECT(&$filter, $table, $column) {
+        if (($result = connect_db($link)) != ret_enum::RET_OK) {
+                return $result;
+        }
+
+        $query = "SELECT `".$column."` FROM `".$table."` WHERE `id` = '".$filter->{'id'}."'";
         error_log("query: $query", 0);
 
         $result = mysqli_query($link, $query);
         $row = mysqli_fetch_array($result);
         mysqli_close($link);
 
-	// Fill requested data
-	$retval['user_mode'] = $row['user_mode'];
-        return json_encode($retval);
+        $ret[$column] = $row[$column];
+        return json_encode($ret);
 }
 
-function addNewRequest(&$var) {
-        if (($result = connect_db($link)) != ret_enum::RET_OK) {
-                return $result;
-        }
-	
-	// Insert new-request into 'workitems' table
-
-	// Search candidates by language
-
-	// Foreach member in this group
-	// 	Update new_job flag, then, they'll be informed soon.
-}
-
-function getInfo(&$var, $table, $column) {
+function UPDATE(&$filter, $table, $column, $value) {
+	$ret = ret_enum::RET_OK;
         if (($result = connect_db($link)) != ret_enum::RET_OK) {
                 return $result;
         }
 
-        $query = "SELECT `".$column."` FROM `".$table."` WHERE `id` = '".$var->{'id'}."'";
+	// TODO: Trace all input $filter
+        $query = "UPDATE `".$table."` SET `".$column."` = '".$value."' WHERE ";
+
+	// Count elements
+	$cnt = 0;
+	foreach($filter as $key) $cnt++;
+
+	// Fill 'WHERE' conditions
+	$idx = 1;
+	foreach($filter as $key => $value) {
+                $query .= "`".$key."` = '".$value."'";
+		if (($idx++) < $cnt)
+			$query .= " and ";
+	}
+        error_log("count: ".$cnt.", query: $query", 0);
+
+        $result = mysqli_query($link, $query);
+	if ($result == false) {
+		error_log("ERROR!!", 0);
+		$ret = ret_enum::RET_DATABASE_ERROR;
+	}
+        mysqli_close($link);
+
+        return $ret;
+}
+
+function SIZE($table, /*OUTPUT*/&$size) {
+        if (($result = connect_db($link)) != ret_enum::RET_OK) {
+                return $result;
+        }
+
+        $query = "SELECT * FROM `".$table."`";
         error_log("query: $query", 0);
 
         $result = mysqli_query($link, $query);
-        $row = mysqli_fetch_array($result);
+        $size = mysqli_num_rows($result);
         mysqli_close($link);
 
-        $retval[$column] = $row[$column];
-        return json_encode($retval);
+        return ret_enum::RET_OK;
 }
 
-function setInfo(&$var, $table, $column, $val) {
-	$retval = ret_enum::RET_OK;
-        if (($result = connect_db($link)) != ret_enum::RET_OK) {
-                return $result;
-        }
-
-        $query = "UPDATE `".$table."` SET `".$column."` = '".$val."' WHERE `id` ='".$var->{'id'}."'";
-        error_log("query: $query", 0);
-
-        $result = mysqli_query($link, $query);
-        $row = mysqli_fetch_array($result);
-        mysqli_close($link);
-
-        return $retval;
-}
 
 ?>
--- main2.php
+++ main2.php
@@ -1,6 +1,13 @@
 <?php
 include("command.php");
 #header('Content-Type: text/html; charset=UTF-8');
+
+function return_if_not_logged_in() {
+        if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true) {
+                echo ret_enum::RET_FAIL;
+                return;
+        }
+}
 session_start();
 
 if (!isset($_SESSION['new_ss'])) {
@@ -20,6 +27,7 @@ if (!isset($json)) {
 
 $obj = json_decode($json);
 $command = $obj->{"command"};
+unset($obj->{'command'});
 error_log("command=$command", 0);
 
 switch ($command) {
@@ -51,41 +59,55 @@ switch ($command) {
 	break;
 
   case 'GET_USER_MODE':
-        if ($_SESSION['logged_in'] != true) {
-                echo ret_enum::RET_FAIL;
-                break;
-        }
+	return_if_not_logged_in();
 	$obj->{'id'} = $_SESSION['logged_in_id']; // Fill id
-	$ret = getInfo($obj, "members", "user_mode");//getUserMode($obj);
+	$ret = SELECT($obj, DB::member_table, "user_mode");//getUserMode($obj);
 	error_log($ret);
 	echo $ret;
 	break;
 
   case 'ADD_NEW_REQUEST': // call by Requester
-	if (!isset($_SESSION['logged_in']) || $_SESSOIN['logged_in'] != true) {
+	return_if_not_logged_in();
+	if (ret_enum::RET_OK != SIZE(DB::work_table, $size)) {
 		echo ret_enum::RET_FAIL;
 		break;
 	}
-	$ret = addNewRequest($obj);
-	echo $ret;
+
+	// Insert new request
+	$obj->{'id'} = $size + 1; // Expect unique request ID
+	$obj->{'requester_id'} = $_SESSION['logged_in_id']; // Fill id
+	$ret = INSERT($obj, DB::work_table);
+	if ($ret != ret_enum::RET_OK) {
+		echo $ret;
+		break;
+	}
+
+	/* Search candidates by language, and 
+	 Set 'new_request' flag to each candidates in member_table*/
+	$filter = (object)array('user_mode' => MEMBER::TRANSLATOR);
+	$filter->{'language'} = $obj->{'target_language'};
+	$result = UPDATE($filter, DB::member_table, "new_request", $obj->{'id'});
+	echo json_encode($obj); // To return request_id
 	break;
 
   case 'GET_NEW_REQUEST': // call by Translator or Reviewer group
-        if ($_SESSION['logged_in'] != true) {
-                echo ret_enum::RET_FAIL;
-                break;
-        }
-        $obj->{'id'} = $_SESSION['logged_in_id']; // Fill id
-        $ret = getInfo($obj, "members", "new_request"); // Get new request which is applicable to me.
-	setInfo($obj, "members", "new_request", 0); // reset it to avoid duplicated notification
-        error_log($ret);
+        return_if_not_logged_in();
+	//$filter->{'id'} = $_SESSION['logged_in_id']; // Fill id
+	$filter = array('id'=>$_SESSION['logged_in_id']);
+        $ret = SELECT($filter, DB::member_table, "new_request"); // Get new request which is applicable to me.
+	UPDATE($filter, DB::member_table, "new_request", 0); // reset it to avoid duplicated notification
+        error_log($ret); // RETURN REQUEST_ID
         echo $ret;
         break;
 
   case 'BID': // call by Translator or Reviewer group
+	return_if_not_logged_in();
+	
 	break;
 
   case 'GET_BID_RESULT': // call by Translator or Reviewer group
+	return_if_not_logged_in();
+
 	break;
 
   default:
